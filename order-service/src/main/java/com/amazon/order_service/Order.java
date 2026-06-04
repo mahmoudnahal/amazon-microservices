f0e275031dcbf099adb654c9acbba5ee5ab3a761
MahmoudAlNahal <you@example.com>
2026-06-04 17:44:05 +0300

    Add all microservices

---
+++ order-service/src/main/java/com/amazon/order_service/Order.java
@@ -0,0 +1,27 @@
+package com.amazon.order_service;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long productId;
+    private Integer quantity;
+    private String status;
+    private Double totalPrice;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public Long getProductId() { return productId; }
+    public void setProductId(Long productId) { this.productId = productId; }
+    public Integer getQuantity() { return quantity; }
+    public void setQuantity(Integer quantity) { this.quantity = quantity; }
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+    public Double getTotalPrice() { return totalPrice; }
+    public void setTotalPrice(Double totalPrice) { this.totalPrice = totalPrice; }
+}
---
+++ product-service/src/main/java/com/amazon/product_service/Product.java
@@ -0,0 +1,51 @@
+//package com.amazon.product_service;
+//
+//
+//
+//import jakarta.persistence.*;
+//import lombok.Data;
+//
+//@Data
+//@Entity
+//@Table(name = "products")
+//public class Product {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    private String name;
+//    private String description;
+//    private double price;
+//    private Integer quantity;
+//}
+
+
+
+package com.amazon.product_service;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private double price;
+    private Integer quantity;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
+    public Integer getQuantity() { return quantity; }
+    public void setQuantity(Integer quantity) { this.quantity = quantity; }
+}
