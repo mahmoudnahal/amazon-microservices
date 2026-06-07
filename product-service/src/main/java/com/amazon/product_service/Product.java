56af5b064327271d46d7cc661ce863450d6f1745
MahmoudAlNahal <you@example.com>
2026-06-07 19:29:31 +0300

    Fix services structure

--- product-service/src/main/java/com/amazon/product_service/Product.java
+++ product-service/src/main/java/com/amazon/product_service/Product.java
@@ -1,26 +1,3 @@
-//package com.amazon.product_service;
-//
-//
-//
-//import jakarta.persistence.*;
-//import lombok.Data;
-//
-//@Data
-//@Entity
-//@Table(name = "products")
-//public class Product {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    private String name;
-//    private String description;
-//    private double price;
-//    private Integer quantity;
-//}
-
-
 
 package com.amazon.product_service;
 
